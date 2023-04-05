9abaa6922db1a405eb2456007716923cc8ad77a9
Benjamin Tan <tanbenjamin123@gmail.com>
2023-04-05 00:55:27 -0400

    added session frl this time

---
+++ session.js
@@ -0,0 +1,19 @@
+// Get the textarea elements
+var textarea1 = document.getElementById("query");
+var textarea2 = document.getElementById("query_input");
+
+// Load the saved values from local storage, if any
+if (localStorage.getItem("savedText1")) {
+textarea1.value = localStorage.getItem("savedText1");
+}
+if (localStorage.getItem("savedText2")) {
+textarea2.value = localStorage.getItem("savedText2");
+}
+
+// Save the values to local storage whenever they change
+textarea1.addEventListener("input", function() {
+localStorage.setItem("savedText1", textarea1.value);
+});
+textarea2.addEventListener("input", function() {
+localStorage.setItem("savedText2", textarea2.value);
+});
