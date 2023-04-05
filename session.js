f4286cf1719a50aaf98e931b55ed7fd9b49ce874
Benjamin Tan <tanbenjamin123@gmail.com>
2023-04-05 02:53:13 -0400

    toggle button html script fix7

--- session.js
+++ session.js
@@ -1,56 +1,60 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // Get the textarea elements
-  var textarea1 = document.getElementById('query');
-  var textarea2 = document.getElementById('query_input');
+  var textarea1 = document.getElementById("query");
+  var textarea2 = document.getElementById("query_input");
 
   // Load the saved values from local storage, if any
-  if (localStorage.getItem('savedText1')) {
-    textarea1.value = localStorage.getItem('savedText1');
+  if (localStorage.getItem("savedText1")) {
+    textarea1.value = localStorage.getItem("savedText1");
   }
-  if (localStorage.getItem('savedText2')) {
-    textarea2.value = localStorage.getItem('savedText2');
+  if (localStorage.getItem("savedText2")) {
+    textarea2.value = localStorage.getItem("savedText2");
   }
 
+  var clearButton1 = document.getElementById("clearButton1");
+  var clearButton2 = document.getElementById("clearButton2");
 
+  var toggleButton = document.getElementById("toggle-Button");
 
-  var clearButton1 = document.getElementById('clearButton1');
-  var clearButton2 = document.getElementById('clearButton2');
-
-  var toggleButton = document.getElementById('toggle-Button');
+  function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 
   var clearEnabled = false;
 
   // Add click event listener to the toggle button
-  toggleButton.addEventListener('click', function () {
+  toggleButton.addEventListener("click", function () {
     clearEnabled = !clearEnabled; // Toggle the flag
     if (clearEnabled) {
-      toggleButton.textContent = 'On'; // Change the text content of the button
+      toggleButton.textContent = "On"; // Change the text content of the button
       toggleButton.disabled = false; // Enable the clear button
     } else {
-      toggleButton.textContent = 'Off'; // Change the text content of the button
+      toggleButton.textContent = "Off"; // Change the text content of the button
       toggleButton.disabled = true; // Disable the clear button
     }
   });
 
   // Save the values to local storage whenever they change
-  textarea1.addEventListener('input', function () {
-    localStorage.setItem('savedText1', textarea1.value);
+  textarea1.addEventListener("input", function () {
+    localStorage.setItem("savedText1", textarea1.value);
   });
-  textarea2.addEventListener('input', function () {
-    localStorage.setItem('savedText2', textarea2.value);
+  textarea2.addEventListener("input", function () {
+    localStorage.setItem("savedText2", textarea2.value);
   });
 
-  clearButton1.addEventListener('click', function () {
+  clearButton1.addEventListener("click", async function () {
     if (clearEnabled) {
-      textarea1.value = '';
-      localStorage.removeItem('savedText1');
+      await delay(2000);
+      textarea1.value = "";
+      localStorage.removeItem("savedText1");
     }
   });
 
-  clearButton2.addEventListener('click', function () {
+  clearButton2.addEventListener("click", async function () {
     if (clearEnabled) {
-      textarea2.value = '';
-      localStorage.removeItem('savedText2');
+      await delay(2000);
+      textarea2.value = "";
+      localStorage.removeItem("savedText2");
     }
   });
 });
