9a48fbe5b66e62af8a80b11587531ff0c9ecaf6b
Nicholas Pop <94801567+Cocovert1@users.noreply.github.com>
2023-04-09 13:19:58 -0400

    Cleaned up the code

--- SqlQuery.php
+++ SqlQuery.php
@@ -1,87 +1,89 @@
 <?php
+//Values to use to connect to the db
 $host = 'zac353.encs.concordia.ca';
 $username = 'zac353_4';
 $password = 'K1cKAl35';
 $dbname = 'zac353_4';
 
+//Checks if a session is already taking place. If not, starts a session.
 if (empty(session_id()) && !headers_sent()) {
     session_start();
 }
 
-// Check if a custom query has been submitted
+//Checking if a custom query has been posted.
 if (isset($_POST['query'])) {
     $query = $_POST['query'];
 
-    // Connect to the database
-    $conn = new mysqli($host, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    //Setting up a connection to the database.
+    $connection = new mysqli($host, $username, $password, $dbname);
+    if ($connection->connect_error) {
+        die("The connection failed");
     }
 
-    // Execute the query
-    $result = $conn->query($query);
+    //Pulling the query and storing it.
+    $result = $connection->query($query);
 
-    // Check if the query was successful
+    //If statement will check if the query has run successfully.
     if ($result) {
-        // Check if the query was a select statement
+        //checking if the query is a select query
         if (stripos(trim($query), "select") === 0) {
+            //Getting the number of columns in the table and setting up the output.
             $output = "<h2>Query Results</h2>";
-
-            // Get the number of columns in the result set
-            $num_columns = mysqli_num_fields($result);
-
+            $nbrofColumns = mysqli_num_fields($result);
             $output .= "<table>";
             $output .= "<tr>";
-            for ($i = 0; $i < $num_columns; $i++) {
+
+            //fetching the column name and setting up the table
+            for ($i = 0; $i < $nbrofColumns; $i++) {
                 $output .= "<th>" . mysqli_fetch_field_direct($result, $i)->name . "</th>";
             }
             $output .= "</tr>";
+
+            //fetching the row and inserting the value
             while ($row = mysqli_fetch_row($result)) {
                 $output .= "<tr>";
-                for ($i = 0; $i < $num_columns; $i++) {
+                for ($i = 0; $i < $nbrofColumns; $i++) {
                     $output .= "<td>" . $row[$i] . "</td>";
                 }
                 $output .= "</tr>";
             }
             $output .= "</table>";
-        } else {
-            $output = "<p>The query was successful.</p>";
+        } else { //if it is not a select query, then we do not need to display a table.
+            $output = "<p>Successful query</p>";
         }
     } else {
-        $output = "<p>Error: " . mysqli_error($conn) . "</p>";
+        $output = "<p>There has been an error, error code is : " . mysqli_error($connection) . "</p>";
     }
 
-    // Store the output in a summary tag list
+    //Adds it to the output history
     $_SESSION['query_output'][] = "<details><summary>Custom Query: $query</summary>$output</details>";
 
-    // Close the database connection
-    $conn->close();
-
-    // Check if an insert, create, or delete query has been submitted
-} else if (isset($_POST['submit'])) {
+    //Closing the connection to the db
+    $connection->close();
+} else if (isset($_POST['submit'])) { // Checking if a query other than select has been run.
     $query = $_POST['query'];
 
-    // Connect to the database
-    $conn = new mysqli($host, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    //Setting up a connection to the database.
+    $connection = new mysqli($host, $username, $password, $dbname);
+    if ($connection->connect_error) {
+        die("Connection failed");
     }
 
     // Execute the query
-    $result = $conn->query($query);
+    $result = $connection->query($query);
 
-    // Check if the query was successful
+    //Since we expect only an insert/create or delete, we know the query is successfull if it returns true.
     if ($result) {
-        $output = "<p>The query was successful.</p>";
+        $output = "<p>Successful query</p>";
     } else {
-        $output = "<p>Error: " . mysqli_error($conn) . "</p>";
+        $output = "<p>There has been an error, error code is : " . mysqli_error($connection) . "</p>";
     }
 
-    // Store the output in a summary tag list
+    //Adds it to the output history
     $_SESSION['query_output'][] = "<details><summary>Insert/Create/Delete Query: $query</summary>$output</details>";
 
-    // Close the database connection
-    $conn->close();
+    //Closing the connection to the db
+    $connection->close();
 }
 
 // Check if the "Clear Session" button has been pressed
@@ -91,6 +93,3 @@ if (isset($_POST['clear_session'])) {
     session_unset();
     session_destroy();
 }
-
-?>
-
